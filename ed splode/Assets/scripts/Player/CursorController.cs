80b49bd60dc5a9f194448c3d9676b41fac4b7368
Becchi2 <96718933+Becchi2@users.noreply.github.com>
2026-08-23 00:36:02 +0200

    some shit

--- ed splode/Assets/scripts/Player/CursorController.cs
+++ ed splode/Assets/scripts/Player/CursorController.cs
@@ -5,6 +5,7 @@ public class CursorController : MonoBehaviour
     //hides the cursor and locks it to the center of the screen
     private void Awake()
     {
+        Cursor.lockState = CursorLockMode.None;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
